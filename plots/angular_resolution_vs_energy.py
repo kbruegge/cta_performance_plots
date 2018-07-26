f586b2e21c230193d1ae26a6b73b98657aab4836
Kai B <kai.bruegge@tu-dortmund.de>
2018-07-26 18:02:50 +0200

    add mulitpliciy cuts to ang res plot

--- plots/angular_resolution_vs_energy.py
+++ plots/angular_resolution_vs_energy.py
@@ -29,16 +29,26 @@ def calculate_distance_theta(df, source_alt=70 * u.deg, source_az=0 * u.deg):
 @click.argument('input_dl3_file', type=click.Path(exists=True))
 @click.option('-o', '--output', type=click.Path(exists=False))
 @click.option('-t', '--threshold', default=0.0)
+@click.option('-m', '--multiplicity', default=2)
+@click.option('--title', default=None)
 @click.option('--reference/--no-reference', default=False)
 @click.option('--complementary/--no-complementary', default=False)
-def main(input_dl3_file, output, threshold, reference, complementary):
+def main(input_dl3_file, output, threshold, reference, complementary, multiplicity, title):
     columns = ['mc_alt', 'mc_az', 'mc_energy', 'az_prediction', 'alt_prediction']
+
     if threshold > 0:
-        columns.extend('gamma_prediction_mean')
+        columns.append('gamma_prediction_mean')
+    if multiplicity > 2:
+        columns.append('num_triggered_telescopes')
 
     df = fact.io.read_data(input_dl3_file, key='array_events', columns=columns).dropna()
+
     if threshold > 0:
         df = df.query(f'gamma_prediction_mean > {threshold}').copy()
+    if multiplicity > 2:
+        df = df.query(f'num_triggered_telescopes >= {multiplicity}').copy()
+
+    # df = df.query(f'(num_triggered_telescopes > 8)').copy()
 
     if complementary:
         color = main_color_complement
@@ -47,13 +57,10 @@ def main(input_dl3_file, output, threshold, reference, complementary):
 
     distance = calculate_distance_theta(df, source_alt=df.mc_alt.values * u.rad, source_az=df.mc_az.values * u.rad)
 
-    bins, bin_center, bin_widths = make_energy_bins(e_min=0.003 * u.TeV, e_max=330 * u.TeV, bins=40)
-
-    df['energy_bin'] = pd.cut(df.mc_energy, bins)
+    bins, bin_center, bin_widths = make_energy_bins(e_min=0.003 * u.TeV, e_max=330 * u.TeV, bins=20)
 
     x = df.mc_energy.values
     y = distance
-    import IPython; IPython.embed()
 
     b_50, bin_edges, binnumber = binned_statistic(x, y, statistic='median', bins=bins)
 
@@ -68,10 +75,11 @@ def main(input_dl3_file, output, threshold, reference, complementary):
     # plt.pcolormesh(bins, bins_y, h.T, cmap='Greys', norm=PowerNorm(0.25))
     log_emin, log_emax = np.log10(bins.min().value), np.log10(bins.max().value)
     log_ymin, log_ymax = np.log10(bins_y.min()), np.log10(bins_y.max())
+    plt.figure(figsize=(8, 5))
     plt.hexbin(x, y, xscale='log', yscale='log', extent=(log_emin, log_emax, log_ymin, log_ymax), cmap=default_cmap, norm=PowerNorm(0.5))
     plt.colorbar()
     plt.plot(bin_centers, b_68, lw=2, color=color, label='68% Percentile')
-    plt.plot(bin_centers, b_50, '--', lw=1, color=color, label='Median')
+    #plt.plot(bin_centers, b_50, '--', lw=1, color=color, label='Median')
 
     if reference:
         path = 'resources/CTA-Performance-prod3b-v1-South-20deg-50h-Angres.txt'
@@ -85,6 +93,8 @@ def main(input_dl3_file, output, threshold, reference, complementary):
     plt.ylim([0.001, 100.8])
     plt.legend()
     plt.tight_layout()
+    if title:
+        plt.title(title)
     if output:
         plt.savefig(output)
     else:
