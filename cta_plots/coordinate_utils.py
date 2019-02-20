bb0794fe5fcfb8529178dfb372935b43595f970c
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2019-02-20 11:40:57 +0100

    more refactorinh

--- cta_plots/coordinate_utils.py
+++ cta_plots/coordinate_utils.py
@@ -1,6 +1,18 @@
 from astropy.coordinates import Angle
 from astropy.coordinates.angle_utilities import angular_separation
 import astropy.units as u
+from cta_plots.mc.spectrum import MCSpectrum, CrabSpectrum, CosmicRaySpectrum, CTAElectronSpectrum
+from fact.io import read_data
+from fact.analysis import li_ma_significance
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+from itertools import product
+from colorama import Fore
+from joblib import Parallel, delayed
+
+ELECTRON_TYPE = 1
+PROTON_TYPE = 0
 
 def calculate_distance_to_true_source_position(df):
     source_az = Angle(df.mc_az.values * u.deg).wrap_at(180 * u.deg)
@@ -22,3 +34,108 @@ def calculate_distance_to_point_source(df, source_alt, source_az):
 
     distance = angular_separation(source_az, source_alt, az, alt).to(u.deg)
     return distance
+
+
+
+def load_signal_events(gammas_path, assumed_obs_time=30 * u.min):
+    crab_spectrum = CrabSpectrum()
+
+    gamma_runs = read_data(gammas_path, key='runs')
+
+    if (gamma_runs.mc_diffuse == 1).any():
+        print(Fore.RED + 'Data given at {} contains diffuse gammas.')
+        print(Fore.RED + 'Need point-like gammas to do theta square plot')
+        print(Fore.RESET)
+        raise ValueError
+    
+    gammas = read_data(gammas_path, key='array_events')
+    
+    mc_production_gamma = MCSpectrum.from_cta_runs(gamma_runs)
+
+    source_az = gammas.mc_az.iloc[0] * u.deg
+    source_alt = gammas.mc_alt.iloc[0] * u.deg
+
+    gammas['theta'] = (
+        calculate_distance_to_point_source(gammas, source_alt=source_alt, source_az=source_az)
+        .to(u.deg)
+        .value
+    )
+
+    gammas['weight'] = mc_production_gamma.reweigh_to_other_spectrum(
+        crab_spectrum, gammas.mc_energy.values * u.TeV, t_assumed_obs=assumed_obs_time
+    )
+
+    return gammas, source_alt, source_az
+
+def load_background_events(protons_path, electrons_path,  source_alt, source_az, assumed_obs_time=30 * u.min):
+    cosmic_ray_spectrum = CosmicRaySpectrum()
+    electron_spectrum = CTAElectronSpectrum()
+
+    protons = read_data(protons_path, key='array_events')
+    protons['theta'] = (
+        calculate_distance_to_point_source(protons, source_alt=source_alt, source_az=source_az)
+        .to(u.deg)
+        .value
+    )
+    proton_runs = read_data(protons_path, key='runs')
+    mc_production_proton = MCSpectrum.from_cta_runs(proton_runs)
+    protons['weight'] = mc_production_proton.reweigh_to_other_spectrum(
+        cosmic_ray_spectrum, protons.mc_energy.values * u.TeV, t_assumed_obs=assumed_obs_time
+    )
+    protons['type'] = PROTON_TYPE
+
+    
+    electron_runs = read_data(electrons_path, key='runs')
+    mc_production_electrons = MCSpectrum.from_cta_runs(electron_runs)
+    electrons = read_data(electrons_path, key='array_events')
+    electrons['weight'] = mc_production_electrons.reweigh_to_other_spectrum(
+        electron_spectrum, electrons.mc_energy.values * u.TeV, t_assumed_obs=assumed_obs_time
+    )
+    electrons['theta'] = (
+        calculate_distance_to_point_source(
+            electrons, source_alt=source_alt, source_az=source_az
+        )
+        .to(u.deg)
+        .value
+    )
+    electrons['type'] = ELECTRON_TYPE
+
+    return pd.concat([protons, electrons], sort=False)
+
+
+def find_best_detection_significance(theta_square_cuts, prediction_cuts, n_jobs, signal_events, background_events, silent=False):
+    iterator = tqdm(
+        product(theta_square_cuts, prediction_cuts),
+        total=len(theta_square_cuts) * len(prediction_cuts),
+        disable=silent,
+    )
+
+    if n_jobs != 1:
+        rs = Parallel(n_jobs=n_jobs, verbose=1, prefer='processes', batch_size=40)(
+            delayed(calculate_significance)(signal_events, background_events, t, p) for t, p in iterator
+        )
+    else:
+        rs = [calculate_significance(signal_events, background_events, t, p) for t, p in iterator]
+    
+    max_index = np.argmax([r[0] for r in rs])
+    best_significance, best_theta_square_cut, best_prediction_cut = rs[max_index]
+
+    return best_prediction_cut, best_theta_square_cut, best_significance
+
+
+def calculate_n_on_n_off(signal_events, background_events, theta_square_cut, prediction_cut):
+    signal_gammalike = signal_events.query(f'gamma_prediction_mean >= {prediction_cut}')
+    bkg_gammalike = background_events.query(f'gamma_prediction_mean >= {prediction_cut}')
+
+    off_bins = np.arange(0, 0.6, theta_square_cut)
+    h, _ = np.histogram(
+        bkg_gammalike['theta'] ** 2, bins=off_bins, weights=bkg_gammalike.weight
+    )
+    n_off = h.mean()
+    n_on = signal_gammalike.query(f'theta <= {np.sqrt(theta_square_cut)}').weight.sum() + n_off
+    return n_on, n_off
+
+
+def calculate_significance(signal_events, background_events, theta_square_cut, prediction_cut):
+    n_on, n_off = calculate_n_on_n_off(signal_events, background_events, theta_square_cut, prediction_cut)
+    return li_ma_significance(n_on, n_off, alpha=1), theta_square_cut, prediction_cut
--- cta_plots/mc/theta_squared.py
+++ cta_plots/mc/theta_squared.py
@@ -10,88 +10,27 @@ from cta_plots.coordinate_utils import (
     calculate_distance_to_point_source,
 )
 from cta_plots.mc.spectrum import MCSpectrum, CrabSpectrum, CosmicRaySpectrum, CTAElectronSpectrum
-from tqdm import tqdm
-from itertools import product
-from colorama import Fore
-from joblib import Parallel, delayed
 import matplotlib.offsetbox as offsetbox
 from fact.io import read_data
-
-ELECTRON_TYPE = 1
-PROTON_TYPE = 0
+from cta_plots.coordinate_utils import load_signal_events, load_background_events, ELECTRON_TYPE
+from cta_plots.coordinate_utils import find_best_detection_significance
 
 
 @click.command()
-@click.argument('gammas_dl3', type=click.Path(exists=True))
-@click.argument('protons_dl3', type=click.Path(exists=True))
-@click.option('-e', '--electrons_path', type=click.Path(exists=True, dir_okay=False))
+@click.argument('gammas_path', type=click.Path(exists=True))
+@click.argument('protons_path', type=click.Path(exists=True))
+@click.argument('electrons_path', type=click.Path(exists=True))
 @click.option('-o', '--output', type=click.Path(exists=False))
 @click.option('-j', '--n_jobs', default=-4)
-def main(gammas_dl3, protons_dl3, electrons_path, output, n_jobs):
+def main(gammas_path, protons_path, electrons_path, output, n_jobs):
 
     t_obs = 5 * u.min
 
-    gammas = read_data(gammas_dl3, key='array_events')
-    gamma_runs = read_data(gammas_dl3, key='runs')
-    mc_production_gamma = MCSpectrum.from_cta_runs(gamma_runs)
+    gammas, source_alt, source_az = load_signal_events(gammas_path, assumed_obs_time=t_obs)
+    background = load_background_events(protons_path, electrons_path, source_alt, source_az,  assumed_obs_time=t_obs,)
 
-    protons = read_data(protons_dl3, key='array_events')
-
-    if (gamma_runs.mc_diffuse == 1).any():
-        print(Fore.RED + 'Need point-like gammas to do theta square plot')
-        print(Fore.RESET)
-        return -1
-
-    source_az = gammas.mc_az.iloc[0] * u.deg
-    source_alt = gammas.mc_alt.iloc[0] * u.deg
-
-    gammas['theta'] = (
-        calculate_distance_to_point_source(gammas, source_alt=source_alt, source_az=source_az)
-        .to(u.deg)
-        .value
-    )
-    protons['theta'] = (
-        calculate_distance_to_point_source(protons, source_alt=source_alt, source_az=source_az)
-        .to(u.deg)
-        .value
-    )
-
-    proton_runs = read_data(protons_dl3, key='runs')
-    mc_production_proton = MCSpectrum.from_cta_runs(proton_runs)
-
-    crab = CrabSpectrum()
-    cosmic = CosmicRaySpectrum()
-
-    gammas['weight'] = mc_production_gamma.reweigh_to_other_spectrum(
-        crab, gammas.mc_energy.values * u.TeV, t_assumed_obs=t_obs
-    )
-    protons['weight'] = mc_production_proton.reweigh_to_other_spectrum(
-        cosmic, protons.mc_energy.values * u.TeV, t_assumed_obs=t_obs
-    )
-    protons['type'] = PROTON_TYPE
-    if electrons_path:
-        electron_spectrum = CTAElectronSpectrum()
-        electron_runs = read_data(electrons_path, key='runs')
-        mc_production_electrons = MCSpectrum.from_cta_runs(electron_runs)
-        electrons = read_data(electrons_path, key='array_events')
-        electrons['weight'] = mc_production_electrons.reweigh_to_other_spectrum(
-            electron_spectrum, electrons.mc_energy.values * u.TeV, t_assumed_obs=t_obs
-        )
-        electrons['theta'] = (
-            calculate_distance_to_point_source(
-                electrons, source_alt=source_alt, source_az=source_az
-            )
-            .to(u.deg)
-            .value
-        )
-        electrons['type'] = ELECTRON_TYPE
-
-        background = pd.concat([protons, electrons], sort=False)
-    else:
-        background = protons
-
-    theta_square_cuts = np.arange(0.01, 0.35, 0.02)
-    prediction_cuts = np.arange(0.4, 1, 0.05)
+    theta_square_cuts = np.arange(0.01, 0.35, 0.05)
+    prediction_cuts = np.arange(0.4, 1, 0.1)
 
     best_prediction_cut, best_theta_square_cut, best_significance = find_best_detection_significance(
         theta_square_cuts, prediction_cuts, n_jobs, gammas, background
@@ -112,12 +51,11 @@ def main(gammas_dl3, protons_dl3, electrons_path, output, n_jobs):
     ax.step(bins[:-1], h_on + h_off.mean(), where='post', label='on events')
     ax.step(bins[:-1], h_off, where='post', label='off events protons')
 
-    if electrons_path:
-        off_electrons = off.query(f'type == {ELECTRON_TYPE}')
-        h_off_electrons, _ = np.histogram(
-            off_electrons['theta'] ** 2, bins=bins, weights=off_electrons.weight
-        )
-        ax.step(bins[:-1], h_off_electrons, where='post', label='off events electrons')
+    off_electrons = off.query(f'type == {ELECTRON_TYPE}')
+    h_off_electrons, _ = np.histogram(
+        off_electrons['theta'] ** 2, bins=bins, weights=off_electrons.weight
+    )
+    ax.step(bins[:-1], h_off_electrons, where='post', label='off events electrons')
 
     ax.set_ylim([0, max(h_on + h_off) * 1.18])
     ax.axhline(y=h_off.mean(), color='C1', lw=1, alpha=0.7)
@@ -132,65 +70,15 @@ def main(gammas_dl3, protons_dl3, electrons_path, output, n_jobs):
         ]
     )
 
-    # these are matplotlib.patch.Patch properties
-    # place a text box in upper left in axes coords
     ob = offsetbox.AnchoredText(textstr, loc=1, prop={'fontsize': 9})
     ob.patch.set_facecolor('lightgray')
     ob.patch.set_alpha(0.1)
     ax.add_artist(ob)
 
-    # plt.text(best_theta_cut**2 + 0.05, max(h_on + h_off + 100), textstr, fontsize=10, verticalalignment='top', bbox=props)
-
-    # plt.legend()
-
-    # s = calculate_significance(gammas, protons, best_theta_cut, best_prediction_cut)
-    # plt.title(f'Significane is {s}')
-
-    # sens = relative_sensitivity(n_on, n_off, alpha=1)
-    # rate = crab._integral(e_min, e_max)
-    # print(f'Integral sensitivity {rate.to("1/(cm^2 s)") * sens}, relative flux {sens}')
-
     if output:
         plt.savefig(output)
     else:
         plt.show()
-
-
-def find_best_detection_significance( theta_square_cuts, prediction_cuts, n_jobs, gammas, background):
-    iterator = tqdm(
-        product(theta_square_cuts, prediction_cuts),
-        total=len(theta_square_cuts) * len(prediction_cuts),
-    )
-
-    if n_jobs != 1:
-        rs = Parallel(n_jobs=n_jobs, verbose=1, prefer='processes', batch_size=40)(
-            delayed(calculate_significance)(gammas, background, t, p) for t, p in iterator
-        )
-    else:
-        rs = [calculate_significance(gammas, background, t, p) for t, p in iterator]
-    
-    max_index = np.argmax([r[0] for r in rs])
-    best_significance, best_theta_square_cut, best_prediction_cut = rs[max_index]
-
-    return best_prediction_cut, best_theta_square_cut, best_significance
-
-
-def calculate_n_on_n_off(gammas, protons, theta_square_cut, prediction_cut):
-    gammas_gammalike = gammas.query(f'gamma_prediction_mean >= {prediction_cut}')
-    protons_gammalike = protons.query(f'gamma_prediction_mean >= {prediction_cut}')
-
-    off_bins = np.arange(0, 0.6, theta_square_cut)
-    h, _ = np.histogram(
-        protons_gammalike['theta'] ** 2, bins=off_bins, weights=protons_gammalike.weight
-    )
-    n_off = h.mean()
-    n_on = gammas_gammalike.query(f'theta <= {np.sqrt(theta_square_cut)}').weight.sum() + n_off
-    return n_on, n_off
-
-
-def calculate_significance(gammas, protons, theta_square_cut, prediction_cut):
-    n_on, n_off = calculate_n_on_n_off(gammas, protons, theta_square_cut, prediction_cut)
-    return li_ma_significance(n_on, n_off, alpha=1), theta_square_cut, prediction_cut
 
 
 if __name__ == '__main__':
